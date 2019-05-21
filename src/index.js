1807e398f44b432af76b52be7aa0789ba8e3f18d
Jonathan Forney <jonathan.forney@gmail.com>
2019-05-20 17:33:56 -0700

    created todo data and populated index html with dummy list items, minor css styling

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import htmlToDOM from './html-to-dom.js';
+import todos from './todo-data.js';
---
+++ src/todo-data.js
@@ -0,0 +1,29 @@
+const todos = [
+    {
+        task: 'Learn templates',
+        completed: true
+    },
+    {
+        task: 'Learn forEach',
+        completed: false,
+    },
+    {
+        task: 'Buy cat food',
+        completed: false,
+    },
+    {
+        task: 'Buy people food',
+        completed: false,
+    },
+    {
+        task: 'Get meds at Wallgreens',
+        completed: false,
+    },
+    {
+        task: 'Help dad with health stuff',
+        completed: false,
+    }
+    
+];
+
+export default todos;
